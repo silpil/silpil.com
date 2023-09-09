4d892d6ff19c9633bd335493cf5ee7ed31182f85
Anton Khlebnikov <>
2023-09-09 10:16:36 +0300

    add margin in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,7 +6,7 @@
         <Logo class="opacity-50 hover:opacity-100 transition z-10" />
 
         <ul class="flex flex-col sm:flex-row mt-20 md:mt-0 text-center md:text-right">
-          <li v-for="{ link, text } in links">
+          <li v-for="{ link, text } in links" class="my-4 md:my-0">
             <a :href="link" class="px-4 py-3 text-sm text-gray-600 transition-colors uppercase duration-300 dark:text-gray-200 hover:text-primary-500 dark:hover:text-primary-400 hover:underline">{{ text }}</a>
           </li>
         </ul>
